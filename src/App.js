581fd615b5e94a60f2e97983f59313f9833d7316
sumitpanwar10 <sumitpanwar.professional@gmail.com>
2023-06-01 21:42:23 +0530

    Nav bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
-
+import React, {useState} from "react";
+import Nav from "./components/Appbar";
+import Paper from '@mui/material/Paper';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import Home from "./components/Home";
 function App() {
+  const[darkMode, setDarkMode]=useState(false)
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: darkMode?'dark':'light',
+    }
+  });
+
+
   return (
-    <div className="bg-red-100">
-      <h1>Hello</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Paper style={{ height: "250vh" }} >
+        <div>
+          <Nav check={darkMode} change={()=>setDarkMode(!darkMode)}/>
+          <Home />
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
